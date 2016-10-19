486d7ce8279584d599c8107ed092bedcda233fbf
Tam <tamborinehands@gmail.com>
2016-10-19 11:44:25 +0100

    rspec helpers added

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,8 +4,6 @@ require 'gilded_rose'
 describe GildedRose do
   # items = [Item.new("foo", 0, 0)]
 
-
-
   describe "#update_quality" do
     it "does not change the name" do
       pending "this test is pending because it's legacy code"
@@ -17,17 +15,18 @@ describe GildedRose do
     attr_reader :items, :rose
 
     before (:each) do
+      include Helpers
       @items = object_double("items")
       @rose = GildedRose.new(items)
     end
 
     it "returns true if has exceptional updating rules" do
-      item = ["Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"].sample
+      item = exceptionals_sample
       expect(rose.item_exceptional(item)).to eq true
     end
 
     it "returns false if item does not have exceptional updating rules" do
-      item = ["Pork", "Rabbits", "Sunglasses"].sample
+      item = non_exceptionals_sample
       expect(rose.item_exceptional(item)).to eq false
     end
   end
---
+++ spec/helpers.rb
@@ -0,0 +1,9 @@
+module Helpers
+  def exceptionals_sample
+    ["Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"].sample
+  end
+
+  def non_exceptionals_sample
+    ["Pork", "Rabbits", "Sunglasses"].sample
+  end
+end
