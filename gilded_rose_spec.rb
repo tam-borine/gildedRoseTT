70c844efdf64551ccc8d55fd46749c498342292a
Tam <tamborinehands@gmail.com>
2016-10-19 10:44:59 +0100

    first commit got skeleton

---
+++ gilded_rose_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'test/unit/assertions'
+require './gilded_rose'
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      items[0].name.should == "fixme"
+    end
+  end
+
+end
