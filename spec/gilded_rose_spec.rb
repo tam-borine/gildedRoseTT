821d4ada9452db4f4ac25aa808110694974abe7a
Tam <tamborinehands@gmail.com>
2016-10-19 11:22:32 +0100

    got failing tests for item_Exceptional method

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,14 +2,36 @@ require 'spec_helper'
 require 'gilded_rose'
 
 describe GildedRose do
+  # items = [Item.new("foo", 0, 0)]
+
+
 
   describe "#update_quality" do
     it "does not change the name" do
       pending "this test is pending because it's legacy code"
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
       items[0].name.should == "fixme"
     end
   end
+
+  describe "#item_exceptional?" do #still need to add Conjured
+    attr_reader :items, :rose
+
+    before (:each) do
+      @items = object_double("items")
+      @rose = GildedRose.new(items)
+    end
+
+    it "returns true if has exceptional updating rules" do
+      item = ["Aged Brie", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert"].sample
+      expect(rose.item_exceptional(item)).to eq true
+    end
+
+    it "returns false if item does not have exceptional updating rules" do
+      item = ["Pork", "Rabbits", "Sunglasses"].sample
+      expect(rose.item_exceptional(item)).to eq false
+    end
+  end
+
+
 
 end
